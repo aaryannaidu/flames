70c998054444e5e97bea7d576d3933401aa780f8
Aaryan Naidu <aaryannaidu@Aaryans-MacBook-Air-5.local>
2025-02-02 09:07:31 +0530

    Fixed files for Render deployment

--- legend/models.py
+++ legend/models.py
@@ -7,7 +7,9 @@ class FlamesResult(models.Model):
     name1 = models.CharField(max_length=100)
     name2 = models.CharField(max_length=100)
     result = models.CharField(max_length=20)
-    
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request 
 
     def __str__(self):
         return f"{self.name1} & {self.name2} → {self.result}"
--- legend/views.py
+++ legend/views.py
@@ -4,7 +4,7 @@ from django import forms
 from .models import FlamesResult
 # Create your views here.
 from django_ratelimit.decorators import ratelimit
-@ratelimit(key='ip', rate='10/m', method='POST', block=True)
+# @ratelimit(key='ip', rate='10/m', method='POST', block=True)
 class FlamesForm(forms.Form):
     name1 = forms.CharField(label="Name 1", widget=forms.TextInput(attrs={'placeholder': 'Enter first name'}))
     name2 = forms.CharField(label="Name 2", widget=forms.TextInput(attrs={'placeholder': 'Enter second name'}))
